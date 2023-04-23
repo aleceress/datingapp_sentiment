4a78ecaf52d9e93a627d08c1a2bba03c1f70fd83
Alessia Cecere <alessia.cecere@studenti.unimi.it>
2023-04-23 10:18:44 +0200

    Add w2v embeddings utility functions

---
+++ utils/embeddings.py
@@ -0,0 +1,51 @@
+from gensim.models import KeyedVectors
+import os
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+import numpy as np
+
+w2v = None
+
+def load_wv2():
+    global w2v
+    if w2v is None:
+        w2v = KeyedVectors.load_word2vec_format("data/word2vec/GoogleNews-vectors-negative300.bin", binary=True)
+
+def get_w2v_embedding(word):
+    load_wv2()
+    return w2v[word]
+
+def get_aspects_embeddings(aspects):
+    if os.path.isfile("data/aspects_embedding.pickle"):
+        with open("data/aspects_embedding.pickle", "rb") as f:
+            return pickle.load(f)
+
+    load_wv2()
+
+    aspects_embedding = {}
+    for aspect in aspects.keys():
+        try:
+            aspects_embedding[aspect] = w2v[aspect]
+        except KeyError:
+            continue
+    with open("data/aspect_embedding.pickle", "wb+") as f:
+            pickle.dump(aspects_embedding, f)
+    return aspects_embedding
+
+
+def get_query_similarities(aspects, query, not_query=None):
+    load_wv2()
+    query_embedding = np.zeros(w2v.vector_size)
+    for word in query.split(" "):
+        query_embedding += get_w2v_embedding(word)
+
+    if not_query:
+        for word in not_query.split(" "):
+            query_embedding -= get_w2v_embedding(word)
+
+    query_embedding = query_embedding/len(query.split(" "))
+    similarity = pd.Series()
+    for w, e in aspects.items():
+        similarity[w] = cosine_similarity(e.reshape(1,-1), query_embedding.reshape(1,-1))[0][0]
+    return similarity
